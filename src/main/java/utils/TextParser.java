7f5ebb4d3c668e98e3833c9a6a1bbe0c0b3826f6
Umut Ay <umut.bora@getir.com>
2021-09-04 08:44:46 +0300

    Updated TextParser

--- src/main/java/utils/TextParser.java
+++ src/main/java/utils/TextParser.java
@@ -1,59 +1,55 @@
 package utils;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Scanner;
 
 public class TextParser {
 
-    public String parser(String initialKeyword, String finalKeyword, String input){
+    public static void main(String[] args) {
+        //Sample execution
+        TextParser textParser = new TextParser();
+        System.out.println(textParser.parser("._","-","ajsKAagq5J3w._CoolButton-sg-j3yaG3 a3TGb"));
+    }
 
+    public String parser(String initialKeyword, String finalKeyword, String input){
         int firstIndex = 0;
 
-        String[] lines = input.split(System.getProperty("line.separator"));
+        Scanner scanner = new Scanner(input);
 
-        for (String line: lines) {
+        while (scanner.hasNextLine()) {
+
+            String line = scanner.nextLine();
 
             if (initialKeyword != null)
                 firstIndex = line.indexOf(initialKeyword) + initialKeyword.length();
 
             if (initialKeyword != null && finalKeyword != null){
-
                 //This is the case where the string will be cut from both sides
                 if (line.contains(initialKeyword) && line.contains(finalKeyword)){
-
                     final int lastIndex = line.indexOf(finalKeyword);
-
+                    scanner.close();
                     return line.substring(firstIndex, lastIndex);
 
                 }
-
             }
             else if (initialKeyword != null){
-
                 //This is the case where only a single side of the string will be cut (left side)
                 if (line.contains(initialKeyword)){
-
+                    scanner.close();
                     return line.substring(firstIndex);
 
                 }
-
             }
             else if (finalKeyword != null){
-
                 //This is the case where only a single side of the string will be cut (right side)
                 if (line.contains(finalKeyword)){
-
                     final int lastIndex = line.indexOf(finalKeyword);
-
+                    scanner.close();
                     return line.substring(0, lastIndex);
 
                 }
-
             }
-
         }
-
+        scanner.close();
         return null;
     }
-
 }
