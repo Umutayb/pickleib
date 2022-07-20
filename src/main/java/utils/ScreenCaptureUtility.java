0b12e65a94ffcbe3299a6a0789906f553b40aaa7
Umut Ay Bora <umut.ay.bora@spritecloud.com>
2022-07-20 13:16:00 +0200

    Update ScreenCaptureUtility.java

--- src/main/java/utils/ScreenCaptureUtility.java
+++ src/main/java/utils/ScreenCaptureUtility.java
@@ -13,12 +13,12 @@ public class ScreenCaptureUtility {
     Printer log = new Printer(ScreenCaptureUtility.class);
     NumericUtilities numeric = new NumericUtilities();
 
-    public File captureScreen(String specName, RemoteWebDriver driver) {
+    public File captureScreen(String name, RemoteWebDriver driver) {
         try {
             log.new Info("Capturing page");
 
-            String name = specName+"#"+numeric.randomNumber(1,10000)+".jpg";
-            File sourceFile = new File("Screenshots");
+            name += "#"+numeric.randomNumber(1,10000)+".jpg";
+            File sourceFile = new File("screenshots");
             File fileDestination  = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
             FileUtils.copyFile(fileDestination, new File(sourceFile, name));
 
