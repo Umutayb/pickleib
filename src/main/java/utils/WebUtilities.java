17df69f3c072a4da433dcd391ec89416e28de695
Umut Ay Bora <umut.ay.bora@spritecloud.com>
2022-08-12 15:58:35 +0200

    Update WebUtilities.java

--- src/main/java/utils/WebUtilities.java
+++ src/main/java/utils/WebUtilities.java
@@ -124,6 +124,26 @@ public abstract class WebUtilities extends Driver { //TODO: Write a method which
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
     }
 
+    public Boolean elementIsPresent(WebElement element, long initialTime){
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+        try {
+            if (!element.isDisplayed()){throw new NoSuchElementException("Element is not displayed!");}
+            return element.isDisplayed();
+        }
+        catch (InvalidElementStateException | StaleElementReferenceException | NoSuchElementException | TimeoutException exception){
+            if (!(System.currentTimeMillis()-initialTime>15000)) {
+                log.new Warning("Recursion! (" + exception.getClass().getName() + ")");
+                elementIsPresent(element, initialTime);
+            }
+            else {
+                driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+                return false;
+            }
+        }
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        return false;
+    }
+
     //This method is for filling an input field, it waits for the element, scrolls to it, clears it and then fills it
     public void clearFillInput(WebElement inputElement, String inputText, Boolean scroll, Boolean verify){
         try {
