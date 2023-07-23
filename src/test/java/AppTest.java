95200a126a6da1174794eaedff5b854dac4b256f
beyazgulsamil <beyazgulsamil@gmail.com>
2023-07-23 16:49:01 +0300

    UnitTest update1
    Assertion message and navigateBrowser's unit test added in appTest.java

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -8,6 +8,7 @@ import pickleib.utilities.element.ElementAcquisition;
 import pickleib.web.driver.PickleibWebDriver;
 import pickleib.web.interactions.WebInteractions;
 
+import static pickleib.enums.Navigation.backwards;
 import static pickleib.web.driver.PickleibWebDriver.driver;
 
 public class AppTest {
@@ -31,35 +32,38 @@ public class AppTest {
     @Test
     public void navigateTest() {
         interactions.navigate(page.trainingUrl);
-        Assert.assertEquals(driver.getCurrentUrl(),page.trainingUrl);
+        Assert.assertEquals("navigateTest FAIL",driver.getCurrentUrl(),page.trainingUrl);
     }
     @Test
     public void setWindowSizeTest(){
-        interactions.setWindowSize(1280,720);
-        //Assert.....
+        int width = 802;
+        int height = 502;
+        interactions.setWindowSize(width-2,height-2);
+        String size = "("+ width +", "+ height +")";
+        Assert.assertEquals("setWindowSizeTest FAIL",driver.manage().window().getSize().toString(),size);
     }
     @Test
-    public void getElementTest() {
-        WebElement element = acquisition.acquireListedElementFromPage("Widgets","toolCards","PageClass", new ObjectRepository());
-        interactions.clickInteraction(element);
-    }
-    @Test
+    public void navigateBrowserTest(){
+        interactions.toPage("buttons");
+        interactions.navigateBrowser(backwards);
+        Assert.assertEquals("navigateBrowserTest FAIL",driver.getCurrentUrl(),page.baseUrl);
+    }@Test
     public void clickTest(){
         interactions.toPage("buttons");
         interactions.clickInteraction(page.clickMeButton);
-        Assert.assertEquals("You have done a dynamic click",page.dynamicClickMessage.getText());
+        Assert.assertEquals("clickTest FAIL","You have done a dynamic click",page.dynamicClickMessage.getText());
     }
     @Test
     public void clickByTextTest(){
         interactions.toPage("buttons");
         interactions.clickByText("Click Me");
-        Assert.assertEquals("You have done a dynamic click",page.dynamicClickMessage.getText());
+        Assert.assertEquals("clickByTextTest FAIL","You have done a dynamic click",page.dynamicClickMessage.getText());
     }
     @Test
     public void scrollInContainerTest(){
         interactions.toPage("elements");
         interactions.scrollInContainer(page.accordionLeftPanel,"Interactions");
-        Assert.assertTrue(page.interactionsAccordionBar.isDisplayed());
+        Assert.assertTrue("scrollInContainerTest FAIL",page.interactionsAccordionBar.isDisplayed());
     }
     @Test
     public void switchWindowByHandleTest(){
@@ -67,7 +71,7 @@ public class AppTest {
         driver.findElement(By.cssSelector("#simpleLink")).click();
         String parentTab = driver.getWindowHandles().toArray()[0].toString();
         interactions.switchWindowByHandle(parentTab);
-        Assert.assertEquals(parentTab,driver.getWindowHandle());
+        Assert.assertEquals("switchWindowByHandleTest FAIL",parentTab,driver.getWindowHandle());
     }
     @Test
     public void switchWindowByIndexTest(){
@@ -75,35 +79,35 @@ public class AppTest {
         driver.findElement(By.cssSelector("#simpleLink")).click();
         interactions.switchWindowByIndex(0);
         String parentTab = driver.getWindowHandles().toArray()[0].toString();
-        Assert.assertEquals(parentTab,driver.getWindowHandle());
+        Assert.assertEquals("switchWindowByIndexTest FAIL",parentTab,driver.getWindowHandle());
     }
     @Test
     public void verifyUrlContainsTest(){
         String text = "qa";
         interactions.verifyUrlContains(text);
-        Assert.assertTrue(driver.getCurrentUrl().contains(text));
+        Assert.assertTrue("verifyUrlContainsTest FAIL",driver.getCurrentUrl().contains(text));
     }
     @Test
     public void verifyCurrentUrlTest(){
         interactions.verifyCurrentUrl(page.baseUrl);
-        Assert.assertEquals(driver.getCurrentUrl(),page.baseUrl);
+        Assert.assertEquals("verifyCurrentUrlTest FAIL",driver.getCurrentUrl(),page.baseUrl);
     }
     @Test
     public void verifyPageTitleTest(){
         String title = "DEMOQA";
         interactions.verifyPageTitle(title);
-        Assert.assertEquals(driver.getTitle(),title);
+        Assert.assertEquals("verifyPageTitleTest FAIL",driver.getTitle(),title);
     }
     @Test
     public void clickWithJSTest(){
         interactions.toPage("buttons");
         interactions.clickWithJS(page.clickMeButton);
-        Assert.assertEquals("You have done a dynamic click",page.dynamicClickMessage.getText());
+        Assert.assertEquals("clickWithJSTest FAIL","You have done a dynamic click",page.dynamicClickMessage.getText());
     }
     @Test
     public void scrollWithJSTest(){
         interactions.toPage("elements");
         interactions.scrollWithJS(page.interactionsAccordionBar);
-        Assert.assertTrue(page.interactionsAccordionBar.isDisplayed());
+        Assert.assertTrue("scrollWithJSTest FAIL",page.interactionsAccordionBar.isDisplayed());
     }
 }
