9d99fa7028fde27fca0df067922a5c438fe08a67
InnaDrapii <i.drapii@gmail.com>
2023-12-20 09:26:12 +0000

    Email timeout

--- src/main/java/pickleib/utilities/email/EmailAcquisition.java
+++ src/main/java/pickleib/utilities/email/EmailAcquisition.java
@@ -1,8 +1,8 @@
 package pickleib.utilities.email;
 
-import context.ContextStore;
 import utils.EmailUtilities;
 import utils.StringUtilities;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -21,15 +21,13 @@ public class EmailAcquisition {
         this.emailInbox = emailInbox;
     }
 
-    static long emailAcquisitionTimeout = Long.parseLong(ContextStore.get("email-acquisition-timeout", "45000"));
-
     public String acquireEmail(EmailUtilities.Inbox.EmailField filterType, String filterKey) {
         emailInbox.log.info("Acquiring & saving email(s) by " +
                 strUtils.highlighted(BLUE, filterType.name()) +
                 strUtils.highlighted(GRAY, " -> ") +
                 strUtils.highlighted(BLUE, filterKey)
         );
-        emailInbox.getEmail(filterType, filterKey, emailAcquisitionTimeout, false, true, true);
+        emailInbox.getEmail(filterType, filterKey, EmailInbox.emailAcquisitionTimeout, false, true, true);
         File dir = new File("inbox");
         String absolutePath = null;
         for (File email : Objects.requireNonNull(dir.listFiles()))
