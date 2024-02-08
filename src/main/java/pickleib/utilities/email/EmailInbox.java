9a4e5557c40f799b20f15b744847f228c4a80834
soldelv <smartconsulting_joana.valle@citizenm.com>
2024-02-08 20:40:27 +0000

    refactored acquired email methods

--- src/main/java/pickleib/utilities/email/EmailInbox.java
+++ src/main/java/pickleib/utilities/email/EmailInbox.java
@@ -1,11 +1,11 @@
 package pickleib.utilities.email;
 
+import collections.Pair;
 import context.ContextStore;
 import lombok.*;
-import org.openqa.selenium.TimeoutException;
 import utils.EmailUtilities;
 import utils.Printer;
-import java.util.concurrent.TimeUnit;
+import utils.reflection.ReflectionUtilities;
 
 import static utils.EmailUtilities.Inbox.EmailField.CONTENT;
 
@@ -13,7 +13,7 @@ import static utils.EmailUtilities.Inbox.EmailField.CONTENT;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@SuppressWarnings({"UnusedReturnValue","unused"})
+@SuppressWarnings({"UnusedReturnValue", "unused"})
 public class EmailInbox {
 
     Printer log = new Printer(EmailInbox.class);
@@ -31,7 +31,7 @@ public class EmailInbox {
     String emailApplicationPassword;
     public long emailAcquisitionTimeout = Long.parseLong(ContextStore.get("email-acquisition-timeout", "45000"));
 
-    public EmailInbox(String host, String port,  String email, String emailApplicationPassword, String secureCon) {
+    public EmailInbox(String host, String port, String email, String emailApplicationPassword, String secureCon) {
         this.host = host;
         this.port = port;
         this.secureCon = secureCon;
@@ -39,7 +39,7 @@ public class EmailInbox {
         this.emailApplicationPassword = emailApplicationPassword;
     }
 
-    public static String getEmail(
+    public static EmailUtilities.Inbox.EmailMessage getEmail(
             String email,
             String emailApplicationPassword,
             String host,
@@ -50,7 +50,7 @@ public class EmailInbox {
             long timeout,
             Boolean print,
             Boolean save,
-            Boolean saveAttachments){
+            Boolean saveAttachments) {
         EmailUtilities.Inbox inbox = getInbox(
                 email,
                 emailApplicationPassword,
@@ -64,33 +64,16 @@ public class EmailInbox {
                 save,
                 saveAttachments
         );
-        return inbox.messages.get(0).get(CONTENT).toString();
+        return inbox.getMessageBy(filter, filterKey);
     }
 
-    public String getEmail(){
-        EmailUtilities.Inbox inbox = getInbox(
-                email,
-                emailApplicationPassword,
-                host,
-                port,
-                secureCon,
-                filter,
-                filterKey,
-                emailAcquisitionTimeout,
-                print,
-                save,
-                saveAttachments
-        );
-        return inbox.messages.get(0).get(CONTENT).toString();
-    }
-
-    public String getEmail(
+    public EmailUtilities.Inbox.EmailMessage getEmail(
             EmailUtilities.Inbox.EmailField filter,
             String filterKey,
             long timeout,
             Boolean print,
             Boolean save,
-            Boolean saveAttachments){
+            Boolean saveAttachments) {
         EmailUtilities.Inbox inbox = getInbox(
                 email,
                 emailApplicationPassword,
@@ -104,7 +87,7 @@ public class EmailInbox {
                 save,
                 saveAttachments
         );
-        return inbox.messages.get(0).get(CONTENT).toString();
+        return inbox.getMessageBy(filter, filterKey);
     }
 
     public static void clearInbox(
@@ -113,70 +96,61 @@ public class EmailInbox {
             String host,
             String port,
             String secureCon
-    ){
+    ) {
         new Printer(EmailInbox.class).info("Flushing email inbox...");
-        new EmailUtilities.Inbox(
+        EmailInbox inbox = new EmailInbox(
                 host,
                 port,
                 email,
                 emailApplicationPassword,
-                secureCon,
-                false,
-                false,
-                false
+                secureCon
         );
+        inbox.clearInbox();
     }
 
-    public void clearInbox(){
+    public void clearInbox() {
         log.info("Flushing email inbox...");
-        new EmailUtilities.Inbox(
+        EmailUtilities.Inbox inbox = new EmailUtilities.Inbox(
                 host,
                 port,
                 email,
                 emailApplicationPassword,
-                secureCon,
-                false,
-                false,
-                false
+                secureCon
         );
+        inbox.load(false, false, false);
     }
 
-    private static EmailUtilities.Inbox getInbox(
+    public static EmailUtilities.Inbox getInbox(
             String email,
             String emailApplicationPassword,
             String host,
             String port,
             String secureCon,
-            EmailUtilities.Inbox.EmailField filter,
-            String filterKey,
             long timeout,
             Boolean print,
             Boolean save,
-            Boolean saveAttachments
-    ){
+            Boolean saveAttachments,
+            Pair<EmailUtilities.Inbox.EmailField, String>... filterPairs) {
         double initialTime = System.currentTimeMillis();
         Printer log = new Printer(EmailInbox.class);
         log.info("Acquiring email...");
-        EmailUtilities.Inbox inbox;
-        do {
-            inbox = new EmailUtilities.Inbox(
-                    host,
-                    port,
-                    email,
-                    emailApplicationPassword,
-                    secureCon,
-                    filter,
-                    filterKey,
-                    print,
-                    save,
-                    saveAttachments
-            );
-            try {TimeUnit.SECONDS.sleep(3);}
-            catch (InterruptedException e) {throw new RuntimeException(e);}
-            if (System.currentTimeMillis() - initialTime > timeout) throw new TimeoutException("Verification email did not arrive!");
-        }
-        while (inbox.messages.size() == 0);
+        EmailUtilities.Inbox inbox = new EmailUtilities.Inbox(
+                host,
+                port,
+                email,
+                emailApplicationPassword,
+                secureCon
+        );
+        ReflectionUtilities.iterativeConditionalInvocation(
+                30,
+                () -> {
+                    inbox.load(print, save, saveAttachments, filterPairs);
+                    return inbox.messages.size() > 0;
+                }
+        );
         log.success("Email(s) acquired!");
         return inbox;
     }
+
+    public static EmailUtilities.Inbox getInbox
 }
