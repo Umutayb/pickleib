32d99b73e054785e950b022ba07a4ac656f9dbaf
Umut Ay Bora <umutaybora@gmail.com>
2024-02-23 11:43:59 +0100

    Update AppTest.java

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -14,7 +14,7 @@ import java.util.List;
 import static pickleib.enums.Navigation.backwards;
 
 public class AppTest {
-    String testWebsiteUrl = "http://0.0.0.0:8080/";
+    String testWebsiteUrl = "http://localhost:8080/";
     WebDriver driver;
     WebInteractions webInteractions;
     Printer log = new Printer(AppTest.class);
