b6f0c1e6fcd48f284dac0fc221e68ccd74aa28f0
Umut Ay Bora <umutaybora@gmail.com>
2024-02-23 06:31:20 +0100

    Update AppTest.java

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -12,7 +12,7 @@ import java.util.List;
 import static pickleib.enums.Navigation.backwards;
 
 public class AppTest extends PageObjectStepUtilities<ObjectRepository> {
-    static String testWebsiteUrl = "http://0.0.0.0:8080/";
+    static String testWebsiteUrl = "http://0.0.0.0:8081/";
     static WebDriver driver;
 
     /**
