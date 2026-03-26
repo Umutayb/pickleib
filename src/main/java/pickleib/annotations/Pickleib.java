fae99de333c91672647555e8adf702c2bcb38c3a
Umut Ay Bora <umutaybora@gmail.com>
2026-03-26 21:25:20 +0100

    feat: add Pickleib annotation definitions

---
+++ src/main/java/pickleib/annotations/Pickleib.java
@@ -0,0 +1,13 @@
+package pickleib.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Pickleib {
+    String[] scanPackages() default {};
+    boolean builtInSteps() default true;
+}
